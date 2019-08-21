ea351a292f4ff7c677ec45f19143afe07e6939c4
Joel Larwood <joel.larwood@uq.net.au>
2019-08-21 12:02:07 +1000

    s2 joined

--- data/s2wrangle.R
+++ data/s2wrangle.R
@@ -1,11 +1,9 @@
 ## Join qualtrics and spotify data 
 
+library(tidyverse)
 
 spotify2 <- read_csv("data/s2_clean.csv") %>% 
- dplyr:filter(remove == 0) %>% 
-  dplyr::select(remove:key_mode, ResponseId, ProlficID)
-
-names(spotify2)
+  dplyr::select(track_name:key_mode, ResponseId)
 
 qual2 <- read_csv("data/QualtricsRAW.csv")
 names(qual2)
@@ -15,11 +13,13 @@ s2_spotify_join <- full_join(qual2, spotify2, by = "ResponseId")
 names(s2_spotify_join)
 
 s2_spotify_join %>% 
-  dplyr::select(ProlificID.x,
-         ProlificID.y, 
-         ResponseId, 
-         contains("track_name"),
-         contains ("artist"),
-         sad.song,
-         valence) %>%
+  dplyr::select(ResponseId, 
+                contains("track_name"),
+                contains ("artist"),
+                sad.song,
+                valence) %>%
   view()
+
+
+write_csv(s2_spotify_join, 
+          path = "data/s2_spotify_join.csv")
